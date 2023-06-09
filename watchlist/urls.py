57ab63ab945a92f4d97067b02e79b7f5a0ae7058
jesvia <jesvia.rofl@gmail.com>
2023-06-09 22:11:15 +0530

    json response individual

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from watchlist.views import movie_list
+from watchlist.views import movie_list, movie_details
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
+    path('<int:pk>', movie_details, name='movie-details'),
 ]
