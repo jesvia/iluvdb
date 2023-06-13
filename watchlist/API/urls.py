d9446de7a904201c3401db9049f619de3b8fb8b4
jesvia <jesvia.rofl@gmail.com>
2023-06-14 01:47:33 +0530

    intro serializers, GET

--- watchlist/API/urls.py
+++ watchlist/API/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from watchlist.views import movie_list, movie_details
+from watchlist.API.views import movie_list, movie_details
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
     path('<int:pk>', movie_details, name='movie-details'),
