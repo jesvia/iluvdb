e07da82eb44f0c71d26bae2f68b2e9515d78e726
jesvia <jesvia.rofl@gmail.com>
2023-06-16 13:20:37 +0530

    serializers - POST

--- watchlist/API/views.py
+++ watchlist/API/views.py
@@ -3,11 +3,20 @@ from watchlist.API.serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all() # QuerySet
-    serializer = MovieSerializer(movies, many = True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all() # QuerySet
+        serializer = MovieSerializer(movies, many = True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+            
 
 @api_view()
 def movie_details(request, pk):
