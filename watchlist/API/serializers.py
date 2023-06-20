c0023a2fa86d111c71f7c430312aea8d8e0100be
jesvia <jesvia.rofl@gmail.com>
2023-06-20 21:58:23 +0530

    validation - field level

--- watchlist/API/serializers.py
+++ watchlist/API/serializers.py
@@ -15,3 +15,9 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name too short man")
+        else:
+            return value
