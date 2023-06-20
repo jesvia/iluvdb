1011e5b3246e97ce4f4f5caf201a5aa9707cd30a
jesvia <jesvia.rofl@gmail.com>
2023-06-20 20:48:52 +0530

    APIView class, PUT, DELETE

--- watchlist/API/urls.py
+++ watchlist/API/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from watchlist.API.views import movie_list, movie_details
+# from watchlist.API.views import movie_list, movie_details
+from watchlist.API.views import MovieListAV, MovieDetailsAV
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-details'),
+    # path('list/', movie_list, name='movie-list'),
+    # path('<int:pk>', movie_details, name='movie-details'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailsAV.as_view(), name='movie-details'),
 ]
