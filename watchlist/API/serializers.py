f19af63a5593b30a67417227df6caa31ae56db2e
jesvia <jesvia.rofl@gmail.com>
2023-06-22 18:26:34 +0530

    custom serializer field - SerializerMethodField

--- watchlist/API/serializers.py
+++ watchlist/API/serializers.py
@@ -3,12 +3,15 @@ from watchlist.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = '__all__'
         # fields = ['id', 'name', 'description']
         # exclude = ['active']
-        
+    def get_len_name(self, object):
+        return len(object.name)
+    
     def validate_name(self, value):
         if len(value) < 2:
             raise serializers.ValidationError("Name too short man")
